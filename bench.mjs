f2dcaef89f18667e4d9d41729fbab7b597d83923
markthree <1801982702@qq.com>
2023-04-15 20:54:10 +0800

    chore: init

---
+++ bench.mjs
@@ -0,0 +1,35 @@
+import { useThermal } from "./dist/index.mjs";
+
+const { invoke } = await useThermal("bench");
+
+function largeComputing(boundary = 1000000000) {
+  let i = 0;
+  while (boundary - i !== 0) {
+    i++;
+  }
+  return i;
+}
+
+const originStart = Date.now();
+const originResult = largeComputing();
+const originDuration = Date.now() - originStart;
+
+const thermalStart = Date.now();
+const thermalResult = await invoke(largeComputing);
+const thermalDuration = Date.now() - thermalStart;
+
+console.log(
+  `origin  - duration: ${originDuration / 1000}s result: ${originResult}`,
+);
+console.log(
+  `thermal - duration: ${thermalDuration / 1000}s result: ${thermalResult}`,
+);
+
+console.log("\n");
+
+console.log(
+  "thermal vs origin -",
+  (originDuration / thermalDuration).toFixed(2) + " ↑",
+);
+
+console.log()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
-import { resolve } from 'path'
-const foo = 1
+import { hash } from "ohash";
+import { asyncExitHook } from "exit-hook";
+import type { AnyFunction } from "m-type-tools";
+import { createStorage } from "unstorage";
+import fsDriver from "unstorage/drivers/fs";
 
-export { resolve, foo }
+const storage = createStorage({
+  driver: fsDriver({ base: "node_modules/.cache/thermal-fn" }),
+});
+
+interface Cache {
+  [hash: string]: any;
+}
+
+export async function useThermal(key: string) {
+  let cache = ((await storage.getItem(key)) as any) || ({} as Cache);
+  // Write only on exit
+  asyncExitHook(
+    async () => {
+      await storage.setItem(key, cache);
+    },
+    {
+      minimumWait: 500,
+    }
+  );
+
+  async function invoke(fn: AnyFunction, ...rest: any[]) {
+    const fnKey = hash([fn, ...rest]);
+    let oldResult = cache[fnKey];
+    if (oldResult) {
+      return oldResult;
+    }
+    const result = await fn(...rest);
+    cache[fnKey] = result;
+    return result;
+  }
+
+  async function refresh() {
+    cache = {};
+  }
+  return {
+    invoke,
+    refresh,
+  };
+}
